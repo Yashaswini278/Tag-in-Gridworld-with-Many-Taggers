71be5ce12dfd612f35bfbbc91ec5095c1bd3c952
Yashaswini <yashu27aug@gmail.com>
2024-09-07 12:40:22 +0530

    added compare expt

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt 
+from environment import GridWorldEnv
 
 def run_episodes(env, policy, num_episodes=1000):
     rewards = []
@@ -15,11 +16,21 @@ def run_episodes(env, policy, num_episodes=1000):
         rewards.append(total_reward)
     return rewards
 
-def plot_average_rewards(rewards, num_episodes=1000, title = None):
-    cumulative_rewards = np.cumsum(rewards)
-    average_rewards = cumulative_rewards / (np.arange(num_episodes) + 1)
-    plt.plot(average_rewards)
+def plot_average_rewards(vi_rewards, td_rewards, num_episodes=1000, title=None):
+    vi_cumulative_rewards = np.cumsum(vi_rewards)
+    vi_average_rewards = vi_cumulative_rewards / (np.arange(num_episodes) + 1)
+    
+    td_cumulative_rewards = np.cumsum(td_rewards)
+    td_average_rewards = td_cumulative_rewards / (np.arange(num_episodes) + 1)
+    
+    plt.figure(figsize=(10, 6))
+    plt.plot(vi_average_rewards, label='Value Iteration')
+    plt.plot(td_average_rewards, label='TD Learning')
     plt.xlabel("Episodes")
     plt.ylabel("Average Reward")
-    plt.title(title)
+    plt.title(title or "Comparison of Value Iteration and TD Learning")
+    plt.legend()
+    plt.grid(True)
     plt.show()
+
+
